8cda1d426f07044f9fdb69e01d2f341a28a94485
Hemiharper <jim.townend@gmail.com>
2025-05-30 21:40:39 +1000

    Add files via upload

---
+++ src/ui/select.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export function Select({ children, className = '' }) {
+  return <div className={className}>{children}</div>;
+}
+
+export function SelectTrigger({ children, className = '' }) {
+  return <div className={className + ' cursor-pointer'}>{children}</div>;
+}
+
+export function SelectContent({ children, className = '' }) {
+  return <div className={className + ' absolute bg-white border rounded shadow-lg z-50'}>{children}</div>;
+}
+
+export function SelectItem({ value, children, className = '' }) {
+  return (
+    <div
+      onClick={() => {}}
+      className={className + ' px-2 py-1 hover:bg-gray-100 cursor-pointer'}
+    >
+      {children}
+    </div>
+  );
+}
+
+export function SelectValue({ placeholder, className = '' }) {
+  return <span className={className + ' text-gray-700'}>{placeholder}</span>;
+}
