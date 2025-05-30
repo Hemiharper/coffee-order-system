e8d52c3f62df63f26e9f8193b32440fa1e0ea011
Hemiharper <jim.townend@gmail.com>
2025-05-30 20:23:53 +1000

    Update baristaview.jsx

--- src/pages/baristaview.jsx
+++ src/pages/baristaview.jsx
@@ -1,5 +1,18 @@
-import CoffeeOrderSystem from "../components/CoffeeOrderSystem";
-
-export default function BaristaView() {
-  return <CoffeeOrderSystem role="barista" />;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width,initial-scale=1" /><title>Barista View</title><link href="/index.css" rel="stylesheet" /></head>
+<body class="bg-blue-600">
+  <div id="root"></div>
+  <script type="module">
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
+    import CoffeeOrderSystem from '/src/components/CoffeeOrderSystem.jsx';
+    // Force barista-only mode
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <React.StrictMode>
+        <CoffeeOrderSystem baristaMode />
+      </React.StrictMode>
+    );
+  </script>
+</body>
+</html>
