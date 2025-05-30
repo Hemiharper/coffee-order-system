4deb270b604a566a326c2cb0559760f9b1f881e4
Hemiharper <jim.townend@gmail.com>
2025-05-30 12:53:30 +1000

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import CoffeeOrderSystem from "./components/CoffeeOrderSystem";
-
-const isBarista = window.location.pathname === "/baristaview";
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CoffeeOrderSystem role={isBarista ? "barista" : "customer"} />
-  </React.StrictMode>
-);
