e535afa36f2488a16ac3471b7b7fcfcd44d79449
Hemiharper <jim.townend@gmail.com>
2025-05-30 20:20:17 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import CoffeeOrderSystem from "./components/CoffeeOrderSystem";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-const isBarista = window.location.pathname === "/baristaview";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CoffeeOrderSystem role={isBarista ? "barista" : "customer"} />
+    <App />
   </React.StrictMode>
 );
