bfd12e737e54c79d921276137e948f190f4a9b3c
Hemiharper <jim.townend@gmail.com>
2025-06-02 22:24:19 +1000

    Delete src 3 directory

--- src 3/src/lib/utils.js
+++
@@ -1,8 +0,0 @@
-export function getOrdersFromStorage() {
-  const stored = localStorage.getItem('coffeeOrders');
-  return stored ? JSON.parse(stored) : [];
-}
-
-export function saveOrdersToStorage(orders) {
-  localStorage.setItem('coffeeOrders', JSON.stringify(orders));
-}
