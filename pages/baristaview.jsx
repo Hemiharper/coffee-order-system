a8f1f8869f5cf8e535deb2cab806f732ffdf6590
Hemiharper <jim.townend@gmail.com>
2025-06-05 18:05:50 +1000

    Add files via upload

---
+++ pages/baristaview.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import CoffeeOrderSystem from '../components/CoffeeOrderSystem.jsx';
+
+export default function BaristaView() {
+  return <CoffeeOrderSystem isBarista={true} />;
+}
