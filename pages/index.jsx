8ea19cee1ae9e9cb746a5d00650476d0e22e3e40
Hemiharper <jim.townend@gmail.com>
2025-06-16 16:49:33 +1000

    Delete pages directory

--- pages/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import CoffeeOrderSystem from '../components/CoffeeOrderSystem.jsx';
-
-export default function CustomerView() {
-  return <CoffeeOrderSystem isBarista={false} />;
-}
